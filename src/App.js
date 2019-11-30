a657f57474f5cd48c8f786b1bb21d1bbd2f076b2
GuRuGu <peter.k.nets@gmail.com>
2019-11-30 19:00:55 +0200

    Finish Add Log Modal

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import M from "materialize-css/dist/js/materialize.min.js";
 
 import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
+import AddBtn from "./components/layout/AddBtn";
+import AddLogModal from "./components/logs/AddLogModal";
 import "./App.css";
 
 const App = () => {
@@ -15,6 +17,8 @@ const App = () => {
     <Fragment>
       <SearchBar />
       <div className="container">
+        <AddBtn />
+        <AddLogModal />
         <Logs />
       </div>
     </Fragment>
