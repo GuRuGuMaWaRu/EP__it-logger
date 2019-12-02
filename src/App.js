7011d679936dd538ffc51e353f06808352908a28
GuRuGu <peter.k.nets@gmail.com>
2019-12-02 18:37:34 +0200

    Minor updates & write modal for Adding a new technician

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SearchBar from "./components/layout/SearchBar";
 import Logs from "./components/logs/Logs";
 import AddBtn from "./components/layout/AddBtn";
 import AddLogModal from "./components/logs/AddLogModal";
+import EditLogModal from "./components/logs/EditLogModal";
+import AddTechModal from "./components/techs/AddTechModal";
 import "./App.css";
 
 const App = () => {
@@ -19,6 +21,8 @@ const App = () => {
       <div className="container">
         <AddBtn />
         <AddLogModal />
+        <EditLogModal />
+        <AddTechModal />
         <Logs />
       </div>
     </Fragment>
